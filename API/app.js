64e6896620ab1d875dd4ba1c8cdaeca6cea9edd6
PIYALI <jhumpa@PIYALIs-MacBook-Pro.local>
2025-07-04 01:26:36 +0530

    Update local project with latest server changes

--- API/app.js
+++ API/app.js
@@ -1,11 +1,17 @@
-require('dotenv').config();
+const fs = require('fs');
+const https = require('https');
 const express = require('express');
 const app = express();
 const userRouter = require('./api/user/user.router');
 
-app.use(express.json()); 
+const sslOptions = {
+  key: fs.readFileSync('./ssl/server.key'),
+  cert: fs.readFileSync('./ssl/server.cert')
+};
 
+app.use(express.json());
 app.use('/api/users', userRouter);
-app.listen(process.env.APP_PORT, () => {
-  console.log('Server is running on PORT : ', process.env.APP_PORT);
+
+https.createServer(sslOptions, app).listen(8443, () => {
+  console.log('🚀 HTTPS server running on port 8443');
 });
--- API/config/database.js
+++ API/config/database.js
@@ -1,3 +1,4 @@
+require('dotenv').config();
 const { createPool } = require('mysql');
 
 const pool = createPool({
